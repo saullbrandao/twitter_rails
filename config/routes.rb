54da26ca3573aceb71614f19525775c01e8869a5
Saull Brandão <saull@outlook.com>
2022-04-19 08:59:51 -0300

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   resources :users
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,5 +12,9 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
   # Add more helper methods to be used by all tests here...
 end

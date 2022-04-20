f1d2391681e70a6c0d9feee6763ef952a3df39bf
Saull Brandão <saull@outlook.com>
2022-04-20 11:16:26 -0300

    Finish user edit, update, index and destroy actions

--- Gemfile
+++ Gemfile
@@ -12,6 +12,9 @@ gem 'jbuilder',   '2.10.0'
 gem 'bootsnap',   '1.7.2', require: false
 gem 'bootstrap-sass',   '3.4.1'
 gem 'bcrypt',     '3.1.13'
+gem 'faker',      '2.11.0'
+gem 'will_paginate',      '3.3.0'
+gem 'bootstrap-will_paginate',      '1.0.0'
 
 group :development, :test do
   gem 'sqlite3', '1.4.2'
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,11 +1,16 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "layout links" do
+  def setup
+    @user = users(:john)
+  end
+
+  test "layout links without login" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
@@ -13,5 +18,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+
+  test "layout links with login" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
   end
 end

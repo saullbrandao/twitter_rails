09eadb54fe1374113a30f660bc40706fd3fe4c7a
Saull Brandão <saull@outlook.com>
2022-04-21 09:29:14 -0300

    Change from sendgrid to mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,15 +68,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'http://vast-dusk-43572.herokuapp.com/'
+  host = 'vast-dusk-43572.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
+    :address => 'smtp.mailgun.org',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+    :password => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain => 'vast-dusk-43572.heroku.com',
     :enable_starttls_auto => true
   }
 

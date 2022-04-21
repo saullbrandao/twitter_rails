bb56e0ff5e63be045ea9a11d3a456fb8b23659c3
Saull Brandão <saull@outlook.com>
2022-04-21 08:27:29 -0300

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  resources :users
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :account_activations, only: [:edit]
 end
